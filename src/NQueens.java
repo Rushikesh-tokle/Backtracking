66ebca31cfc43c8d838f113d495e229e36748e76
rushi <rushikesh.tokle@gmail.com>
2025-02-06 12:49:39 +0530

    NQueens

--- src/NQueens.java
+++ src/NQueens.java
@@ -4,11 +4,31 @@ public class NQueens {
             printBoard(board);
             return;
         }
-   for(int j=0;j< board.length;j++){
-       board[row][j]='Q';
-       nQueens(board,row+1);
-       board[row][j]='X';
+   for(int j=0;j< board.length;j++) {
+       if (isSafe(board, row, j)) {
+           board[row][j] = 'Q';
+           nQueens(board, row + 1);
+           board[row][j] = 'X';
+       }
    }
+    }
+    public static boolean isSafe(char board[][],int row,int col){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for(int i=row-1,j=col+1;i>=0  && j< board[0].length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
     }
     public static void printBoard(char board[][]){
 
@@ -22,7 +42,7 @@ public class NQueens {
 
     }
     public static void main(String[] args) {
-        int n=2;
+        int n=4;
         char board[][]=new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
